381638c034d9c225bac5d292bfb57365a2182372
root <root@DESKTOP-1EKB8BA>
2024-08-21 10:11:11 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import "./App.css";
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+        data => {setBackendData(data)}
+    )
+  }, [])
+
+  return (
+      <div>
+        {(typeof backendData.users === 'undefined') ? (
+          <p>Loading ...</p>
+        ) : (
+          backendData.users.map((user, i) => {
+            return <p key={i}>{user}</p>;
+          })
+        )}
+      </div>
+    );
+}
+
+export default App;
