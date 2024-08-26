014f761a653b0cd87287b674d67be30a9a69602b
root <root@DESKTOP-1EKB8BA>
2024-08-26 14:07:45 +0300

    Color update

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "../styles/navbar.css";
+import { ReactComponent as HomeIcon } from '../svgs/home.svg';
+import { ReactComponent as LogInIcon } from '../svgs/logIn.svg';
+import { ReactComponent as CatalogIcon } from '../svgs/photoAlbum.svg';
 
 function Navbar() {
     return (
         <div className="navbar">
             <div className="navbar-title">Navbar</div>
             <div className="items">
-                <button className="navbar-item">Home</button>
-                <button className="navbar-item">Catalog</button>
-                <button className="navbar-item">Contact</button>
+                <button className="navbar-item">Home
+                <HomeIcon width="24" height="24" />
+                </button>
+                <button className="navbar-item">Catalog
+                <CatalogIcon width="24" height="24" />
+                </button>
+                <button className="navbar-item">Log In
+                <LogInIcon width="24" height="24" />
+                </button>
             </div>
         </div>
     );
